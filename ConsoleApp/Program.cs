35469165036e98576cb57f93592b6a26e67ae581
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-04-17 11:57:43 +0900

    Console.SetCuresorPosition 연습

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //절댓값 기준
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출2");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("메서드 호출3");
         }
     }
 }
