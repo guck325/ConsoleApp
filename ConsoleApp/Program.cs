61e3707b243174764baa67ccc3221b56b17bdad1
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-04-17 12:20:56 +0900

    여러가지 콘솔 메서드 사용해서 스네이크 움직임 구현

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,23 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
 namespace ConsoleApp
 {
+    
     internal class Program
     {
+        private static readonly int SnakeStateTotal = 3;
+        private const int SnakeState1 = 0;
+        private const int SnakeState2 = 1;  
+        private const int SnakeState3 = 2;
+        private static readonly int SnakeMoveCount = 50;
+        private static readonly int SnakeRow = 5;
+        private static readonly int SnakeSleepTime = 1000;
         static void Main(string[] args)
         {
-            //절댓값 기준
-            Console.Write("메서드 호출");
-            Thread.Sleep(1000);
-            Console.SetCursorPosition(5, 5);
-            Console.Write("메서드 호출2");
-            Thread.Sleep(2000);
-            Console.SetCursorPosition(0, 0);
-            Console.Write("메서드 호출3");
+            int x = SnakeState2;
+            while (x<50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, SnakeStateTotal);
+
+                switch (x % SnakeRow)
+                {
+                    case SnakeState1:
+                        Console.WriteLine("__@");
+                        break;
+                    case SnakeState2:
+                        Console.WriteLine("_^@");
+                        break;
+                    case SnakeState3:
+                        Console.WriteLine("^_@");
+                        break;
+                }
+
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("__@");
+                }else if(x%3 ==1)
+                {
+                    Console.WriteLine("_^@");
+                }
+                else
+                {
+                    Console.WriteLine("^_@");
+                }
+
+                Thread.Sleep(SnakeSleepTime);
+                x++;
+            }
         }
     }
 }
